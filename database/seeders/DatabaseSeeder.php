3107ec2a93690f8719fbce6b7f237c6c8aabd9e7
Lilian Levano <zey.lilianlevano@gmail.com>
2026-06-03 10:03:04 +0200

    ADD: role_id to User model, changed its migration, made a test user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+
 
         User::factory()->create([
-            'name' => 'Test User',
+            'first_name' => 'Test',
+            'last_name' => 'User',
             'email' => 'test@example.com',
+            'role_id' => 'Admin',
+            'password' => bcrypt('password'),
+            'is_admin' => true,
         ]);
     }
 }
