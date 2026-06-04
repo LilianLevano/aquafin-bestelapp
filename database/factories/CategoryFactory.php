a345859ae3ec76fb2479eddfefd1179f80d304bb
Lilian Levano <zey.lilianlevano@gmail.com>
2026-06-04 10:18:14 +0200

    ADD: Category model, migration, factory

---
+++ database/factories/CategoryFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Category;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Category>
+ */
+class CategoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->unique()->randomElement(["Bevestigingsmateriaal",
+                "Persoonlijke beschermingsmiddelen (PBM)",
+                "Gereedschap (manueel & elektrisch)",
+                "Technische onderhoudsmaterialen",
+                "Specifieke Aquafin/riolering gerelateerde tools",
+                "Diversen / Verbruiksgoederen"  ]),
+        ];
+    }
+}
