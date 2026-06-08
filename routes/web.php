0cbe494c058e30415d11bcfab53040744533aab7
meryemduga <meryemduga10@gmail.com>
2026-06-08 15:42:10 +0200

    Merge branch 'Aquafin-breeze'

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AanvraagController;
 
@@ -17,15 +19,24 @@ Route::prefix('admin')->name('admin.')->group(function () {
     Route::resource('rollen', \App\Http\Controllers\Admin\AdminRollenController::class);
 });
 
-Route::middleware('auth')->group(function () {
-});
 Route::get('/', function () {
     return view('welcome');
-})->name('home');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 Route::get('/admin/catalogus/materiaal', function () {
     return view('admin-catalogus-materiaal');
 });
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 Route::get('/catalogus', function () {
     return view('materiaal-catalogus');
