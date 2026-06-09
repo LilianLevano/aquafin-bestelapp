823b12b85e31970c7057ad4e51f33cae0bbbfda5
Lilian Levano <zey.lilianlevano@gmail.com>
2026-06-09 14:39:00 +0200

    ADD: Store bestelling method

--- database/migrations/0001_01_01_000005_create_users_table.php
+++ database/migrations/0001_01_01_000005_create_users_table.php
@@ -17,6 +17,7 @@ return new class extends Migration
             $table->string('last_name');
             $table->string('email')->unique();
             $table->foreignId('role_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('site_id')->nullable()->constrained()->nullOnDelete();
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
