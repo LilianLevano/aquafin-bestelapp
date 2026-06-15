a4ac3c33240db825fadad03b786a2325b81c166f
Lilian Levano <zey.lilianlevano@gmail.com>
2026-06-15 09:56:32 +0200

    ADD: fuzzy seach bar to orders index

--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -4,10 +4,6 @@
     <h1>Bestellingen Overzicht</h1>
 
     <div class="filter-zone">
-        <div class="filter-item">
-            <label>Zoeken</label>
-            <input type="text" placeholder="Zoek op woord...">
-        </div>
 
         <div class="filter-item">
             <label>Datum</label>
@@ -28,9 +24,15 @@
         <button class="btn-primary">Filter</button>
     </div>
 
+    <div class="mb">
+        <input type="text" id="search-order" placeholder="Zoek een bestelling op leverplaats of gebruikersnaam..." autocomplete="off"
+               style="margin-bottom: 0; padding: .5rem; width: 100%; position: relative;">
+        <ul id="search-suggestions" style=" list-style: none; margin-bottom: 10px; padding: 0; border: 1px solid #ccc; border-top: none; position: absolute; background: white; width: 40%; z-index: 100; display: none; "></ul>
+    </div>
+
     <button><a href="{{route('orders.create')}}">Plaats een nieuwe bestelling</a> </button>
 
-    <table class="manager-table">
+    <table class="manager-table" id="orders-table">
         <thead>
             <tr>
                 <th>ID</th>
@@ -44,12 +46,12 @@
         </thead>
 
 
-        <tbody>
+        <tbody id="orders-tbody">
 
 
         @foreach($orders as $order)
-            <tr>
-                <td>{{$order->id}}</td>
+            <tr data-id="{{ $order->id }}" data-firstname="{{ $order->user->first_name }}" data-lastname="{{ $order->user->last_name }}" data-deliverysite="{{$order->site->description}}">
+                <td >{{$order->id}}</td>
                 <td  class="extra-information-order">{{$order->user->first_name . ' ' . $order->user->last_name  }}</td>
                 <td class="extra-information-order">
                     {{ $order->materials->take(3)->map(fn($m) => $m->name . ' (x' . $m->pivot->quantity . ')')->implode(', ') . ($order->materials->count() > 3 ? ', ...' : '') }}
